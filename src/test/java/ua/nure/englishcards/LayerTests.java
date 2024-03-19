b2580899311821e5c1c0e6cbcb29649526debf3f
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-03-19 14:22:13 +0200

    Format code

--- src/test/java/ua/nure/englishcards/LayerTests.java
+++ src/test/java/ua/nure/englishcards/LayerTests.java
@@ -1,58 +1,61 @@
 package ua.nure.englishcards;
 
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+import static java.lang.String.format;
+
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
 import org.junit.jupiter.api.Test;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
-import static java.lang.String.format;
 
-
+/**
+ * The class is responsible for testing the architecture characteristics.
+ */
 public class LayerTests {
 
-    private final String ROOT_PATH = "ua.nure.englishcards";
+  private static final String ROOT_PATH = "ua.nure.englishcards";
 
-    @Test
-    public void shouldCheckThatApiLayerIsNotUsedInOtherLayers() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages(ROOT_PATH);
+  @Test
+  public void shouldCheckThatApiLayerIsNotUsedInOtherLayers() {
+    JavaClasses importedClasses = new ClassFileImporter().importPackages(ROOT_PATH);
 
-        ArchRule rule = classes()
-                .that().resideInAPackage("..api..")
-                .should().onlyBeAccessed().byAnyPackage("..api..")
-                .allowEmptyShould(true);
+    ArchRule rule = classes()
+        .that().resideInAPackage("..api..")
+        .should().onlyBeAccessed().byAnyPackage("..api..")
+        .allowEmptyShould(true);
 
-        rule.check(importedClasses);
-    }
+    rule.check(importedClasses);
+  }
 
-    @Test
-    public void shouldCheckThatServiceLayerCanBeUsedInApiAndServiceLayers() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages(ROOT_PATH);
+  @Test
+  public void shouldCheckThatServiceLayerCanBeUsedInApiAndServiceLayers() {
+    JavaClasses importedClasses = new ClassFileImporter().importPackages(ROOT_PATH);
 
-        ArchRule rule = classes()
-                .that().resideInAPackage("..service..")
-                .should().onlyBeAccessed().byAnyPackage("..api..", "..service..")
-                .allowEmptyShould(true);
+    ArchRule rule = classes()
+        .that().resideInAPackage("..service..")
+        .should().onlyBeAccessed().byAnyPackage("..api..", "..service..")
+        .allowEmptyShould(true);
 
-        rule.check(importedClasses);
-    }
+    rule.check(importedClasses);
+  }
 
-    @Test
-    public void shouldCheckThatPersistenceLayerCanBeUsedInServiceLayer() {
-        JavaClasses importedClasses = new ClassFileImporter().importPackages(ROOT_PATH);
+  @Test
+  public void shouldCheckThatPersistenceLayerCanBeUsedInServiceLayer() {
+    JavaClasses importedClasses = new ClassFileImporter().importPackages(ROOT_PATH);
 
-        ArchRule rule = classes()
-                .that().resideInAPackage("..persistence..")
-                .should().onlyBeAccessed().byAnyPackage("..persistence..", "..service..")
-                .allowEmptyShould(true);
+    ArchRule rule = classes()
+        .that().resideInAPackage("..persistence..")
+        .should().onlyBeAccessed().byAnyPackage("..persistence..", "..service..")
+        .allowEmptyShould(true);
 
-        rule.check(importedClasses);
-    }
+    rule.check(importedClasses);
+  }
 
-    @Test
-    public void shouldNotHaveCycleDependenciesInClasses() {
-        slices().matching(format("%s.(*)..", ROOT_PATH)).should().beFreeOfCycles();
-    }
+  @Test
+  public void shouldNotHaveCycleDependenciesInClasses() {
+    slices().matching(format("%s.(*)..", ROOT_PATH)).should().beFreeOfCycles();
+  }
 
 }
