67b83907a614d1e4b2a0c719ce6d353cdc6b8c00
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-03-19 16:25:18 +0200

    Add the EnableTransactionManagement annotation into the EnglishCardsApplication class

--- src/main/java/ua/nure/englishcards/EnglishCardsApplication.java
+++ src/main/java/ua/nure/englishcards/EnglishCardsApplication.java
@@ -2,11 +2,13 @@ package ua.nure.englishcards;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
  * The main class that starts application.
  */
 @SpringBootApplication
+@EnableTransactionManagement
 public class EnglishCardsApplication {
 
   public static void main(String[] args) {
