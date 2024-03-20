2235df8df69846a6cf4f93c190d0a8d84b6a0e87
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-03-20 08:41:10 +0200

    Add a new endpoint for getting user by uuid

--- src/main/java/ua/nure/englishcards/api/UserController.java
+++ src/main/java/ua/nure/englishcards/api/UserController.java
@@ -1,10 +1,15 @@
 package ua.nure.englishcards.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import ua.nure.englishcards.service.NotFoundUserException;
 import ua.nure.englishcards.service.UserService;
 import ua.nure.englishcards.service.model.NewUserModel;
 import ua.nure.englishcards.service.model.UserModel;
@@ -30,6 +35,24 @@ public class UserController {
     UserModel userModel = userService.addNewUser(newUserModel);
 
     return ResponseEntity.ok(userModel);
+  }
+
+  /**
+   * Receives a user by uuid If user was not found
+   * the endpoint throws the exception with status code Not found.
+   *
+   * @param uuid is user id
+   * @return {@code UserModel} or {@code ResponseStatusException}
+   */
+  @GetMapping("/{uuid}")
+  public ResponseEntity<UserModel> getUserByUuid(@PathVariable("uuid") String uuid) {
+    try {
+      UserModel userModel = userService.getUserByUuid(uuid);
+      return ResponseEntity.ok(userModel);
+    } catch (NotFoundUserException notFoundUserException) {
+
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, notFoundUserException.getMessage());
+    }
   }
 
 }
