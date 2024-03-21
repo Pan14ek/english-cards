9ef51cb9d343104e721b2b9c6284944e369472d3
Oleksii Makieiev <alexmakeev2703@gmail.com>
2024-03-21 09:39:26 +0200

    Add a new version of the users table

---
+++ src/main/resources/db/migration/V2__ddl.sql
@@ -0,0 +1,8 @@
+CREATE TABLE if not exists users
+(
+    id              UUID         not null primary key,
+    email           varchar(255) not null unique,
+    nickname        varchar(32)  not null unique,
+    password        varchar(255) not null,
+    registered_date timestamp default now()
+);
